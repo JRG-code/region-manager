23d70b607727af9271cdef9b4fe87df1f122cc60
JRG-code <65093905+JRG-code@users.noreply.github.com>
2025-12-29 20:41:37 +0100

    Bump version to 1.2.9

--- region-manager.php
+++ region-manager.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Region Manager
  * Plugin URI:        https://datalab.com/region-manager
  * Description:       Manage WooCommerce products and pricing across multiple regions with country-specific settings.
- * Version:           1.2.8
+ * Version:           1.2.9
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Region Manager Team
@@ -32,7 +32,7 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Current plugin version.
  */
-define( 'RM_VERSION', '1.2.8' );
+define( 'RM_VERSION', '1.2.9' );
 
 /**
  * Plugin directory path.
